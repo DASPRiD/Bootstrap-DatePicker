c164f18c2fa683056211a24d3c3a7d64a08fddfc
Ben Scholzen <mail@dasprids.de>
2014-06-06 22:08:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Bootstrap Datepicker
+
+I looked around a lot to find some nice datepicker for bootstrap, but the major
+problem was that they were either lacking crucial features or simply not
+maintained anymore with a lot of open issues. Thus I decided to just roll my
+own one. To keep the datepicker as simple as possible, it's using Moment.js
+for handling all the date-specific stuff and the i18n.
+
+The most important feature about this datepicker is that all values transfered
+to your backend and coming from it are always formatted as ISO dates, so you
+don't have to do any complicated parsing on your server.
+
+## Installation
+
+First, grab the latest version of Moment.js from here:
+http://momentjs.com/
+
+If you only need support for the English locale, you are fine with the smaller
+moment.min.js file, else go with moment-with-lang.min.js. Right now you cannot
+change the locale of the datepicker locally, so just change the locale of
+Moment.js globally:
+
+```js
+moment.lang('de')
+```
+
+Next, just load up bootstrap-datepicker.css and bootstrap-datepicker.js. The
+final step is to initialize it. All you need to do is set up your input, for
+instance:
+
+```html
+<input type="text" class="form-control datepicker" value="2014-01-01"/>
+```
+
+Then you can just let the jQuery plugin roll over it:
+
+```js
+$('input.datepicker').bootstrapDatePicker();
+```
+
+You can also pass a few options to the initializer:
+
+- defaultToday (bool, ```false```): whether the input should default to today
+  when the value is empty.
+- dateFormat (string, ```L```): date format displayed in the input field to the
+  user. See http://momentjs.com/docs/#/displaying/format/
+
+Instead of passing these values into the initializer, you can also plant them
+onto the input:
+
+```html
+<input type="text" class="form-control datepicker" data-default-today="true" data-date-format="YY/MM/DD"/>
+```
+
+```js
+$('input.datepicker').each(function(){
+    var datepicker = $(this);
+    datepicker.bootstrapDatePicker(datepicker.data());
+});
+```
+
+Last but not least, you are also able to get and set the date of the datepicker
+via API calls. The setter will expect either a ```Moment``` object or
+```null```, but you can also pass in a string and Moment.js will try to parse
+it. The getter will always return either a ```Moment``` object or ```null```.
+
+```js
+var date = $('#some-datepicker-input').bootstrapDatePicker('getDate');
+$('#some-datepicker-input').bootstrapDatePicker('setDate', date);
+```
+
---
+++ bootstrap-datepicker.js
@@ -0,0 +1,254 @@
+(function($){
+    'use strict';
+
+    var DatePicker = function(element, options){
+        this.options = $.extend({}, this.defaults, options);
+        this.originalInput = $(element);
+        this.initDatePicker();
+    };
+
+    DatePicker.prototype = {
+        constructor: DatePicker,
+
+        defaults: {
+            defaultToday: false,
+            dateFormat: 'L'
+        },
+
+        initDatePicker: function(){
+            this.input = this.originalInput.clone();
+
+            this.originalInput.removeAttr('required');
+            this.originalInput.hide();
+            this.input.removeAttr('name');
+            this.input.attr('readonly', 'readonly');
+            this.input.val('');
+
+            this.group = $('<div class="input-group bootstrap-datepicker"/>');
+            this.originalInput.after(this.group);
+            this.group.append('<span class="input-group-addon"><span class="glyphicon glyphicon-calendar"/></span>');
+            this.group.append(this.input);
+            this.group.append('<span class="input-group-btn"><button class="btn btn-default" type="button"><span class="glyphicon glyphicon-remove"/></button></span>');
+
+            this.date = null;
+
+            if (this.originalInput.val() !== '') {
+                this.date = moment(this.originalInput.val(), 'YYYY-MM-DD');
+
+                if (!this.date.isValid()) {
+                    this.date = null;
+                }
+            } else if (this.options.defaultToday) {
+                this.date = moment();
+            } else {
+                this.date = null;
+            }
+
+            this.updateInputs();
+            this.calendar = null;
+            var datePicker = this;
+
+            this.group.on('click', function(){
+                datePicker.initCalendar.call(datePicker);
+            });
+
+            this.group.find('button').on('click', function(){
+                datePicker.unsetValue.call(datePicker);
+                return false;
+            });
+        },
+
+        initCalendar: function(){
+            if (this.input.is('.disabled') || this.input.attr('disabled') || this.calendar !== null) {
+                return;
+            }
+
+            this.calendar = $(
+                '<div class="bootstrap-datepicker-calendar">' +
+                '    <table class="table table-bordered">' +
+                '       <thead>' +
+                '           <tr class="months">' +
+                '               <th colspan="4" class="month">' +
+                '                   <a class="previous" href="#"><i class="glyphicon glyphicon-chevron-left"></i></a>' +
+                '                   <span/>' +
+                '                   <a class="next" href="#"><i class="glyphicon glyphicon-chevron-right"></i></a>' +
+                '               </th>' +
+                '               <th colspan="3" class="year">' +
+                '                   <a class="previous" href="#"><i class="glyphicon glyphicon-chevron-left"></i></a>' +
+                '                   <span/>' +
+                '                   <a class="next" href="#"><i class="glyphicon glyphicon-chevron-right"></i></a>' +
+                '               </th>' +
+                '           </tr>' +
+                '           <tr class="weekdays"/>' +
+                '       </thead>' +
+                '       <tbody/>' +
+                '    </table>' +
+                '</div>'
+            );
+
+            var weekdays = this.calendar.find('table > thead > tr.weekdays');
+            var weekdate = moment();
+
+            for (var weekday = 0; weekday < 7; weekday++) {
+                weekdate.weekday(weekday);
+                weekdays.append($('<th>').text(weekdate.format('ddd')));
+            }
+
+            this.group.append(this.calendar);
+            var datePicker = this;
+
+            var date = this.date === null ? moment() : moment(this.date);
+            date.day(1);
+
+            this.calendar.data('date', date);
+            this.updateCalendar();
+
+            this.calendar.find('table > thead > tr > th.month > a.previous').on('click', function(){
+                date.month(date.month() - 1);
+                datePicker.updateCalendar.call(datePicker);
+            });
+
+            this.calendar.find('table > thead > tr > th.month > a.next').on('click', function(){
+                date.month(date.month() + 1);
+                datePicker.updateCalendar.call(datePicker);
+            });
+
+            this.calendar.find('table > thead > tr > th.year > a.previous').on('click', function(){
+                date.year(date.year() - 1);
+                datePicker.updateCalendar.call(datePicker);
+            });
+
+            this.calendar.find('table > thead > tr > th.year > a.next').on('click', function(){
+                date.year(date.year() + 1);
+                datePicker.updateCalendar.call(datePicker);
+            });
+
+            this.calendar.find('table > tbody').on('click', 'td:not(.off)', function(){
+                datePicker.selectValue.call(datePicker, $(this).data('day'));
+            });
+
+            this.closeHandler = function(event){
+                if (!$(event.target).closest('.bootstrap-datepicker').is(datePicker.group)) {
+                    datePicker.destroyCalendar.call(datePicker);
+                    $(document).off('click.bootstrap-datepicker', null, datePicker.closeHandler);
+                }
+            };
+
+            $(document).on('click.bootstrap-datepicker', this.closeHandler);
+        },
+
+        selectValue: function(day){
+            this.date = this.calendar.data('date');
+            this.date.date(day);
+            this.updateInputs();
+            this.destroyCalendar();
+        },
+
+        unsetValue: function(){
+            this.date = null;
+            this.updateInputs();
+            this.destroyCalendar();
+        },
+
+        updateCalendar: function(){
+            var date = this.calendar.data('date');
+
+            this.calendar.find('table > thead > tr > th.month > span').text(date.format('MMMM'));
+            this.calendar.find('table > thead > tr > th.year > span').text(date.format('YYYY'));
+
+            var days = this.calendar.find('table > tbody').html('');
+
+            var numDaysCurrentMonth  = date.month(date.month() + 1).date(0).date();
+            var lastWeekday          = date.weekday();
+            var numDaysPreviousMonth = date.date(0).date();
+            var firstWeekday         = date.date(date.date() + 1).weekday();
+
+            var day, weekday, row = '';
+
+            for (weekday = 0; weekday < firstWeekday; weekday++) {
+                row += '<td class="off">' + (numDaysPreviousMonth - (firstWeekday - weekday - 1)) + '</td>';
+            }
+
+            var today = moment();
+
+            for (day = 1; day <= numDaysCurrentMonth; day++) {
+                if (today.year() === date.year() && today.month() === date.month() && today.date() === day) {
+                    row += '<td class="today" data-day="' + day + '">' + day + '</td>';
+                } else {
+                    row += '<td data-day="' + day + '">' + day + '</td>';
+                }
+
+                if (++weekday === 7) {
+                    weekday = 0;
+                    days.append('<tr>' + row + '</tr>');
+                    row = '';
+                }
+            }
+
+            if (weekday > 0) {
+                for (day = 1; weekday < 7; weekday++, day++) {
+                    row += '<td class="off">' + day + '</td>';
+                }
+                days.append('<tr>' + row + '</tr>');
+            }
+        },
+
+        destroyCalendar: function(){
+            if (this.calendar === null) {
+                return;
+            }
+
+            this.calendar.remove();
+            this.calendar = null;
+            this.closeHandler = null;
+        },
+
+        getDate: function(){
+            return this.date === null ? null : moment(this.date);
+        },
+
+        setDate: function(date){
+            this.date = !date ? null : moment(date);
+
+            if (!this.date.isValid()) {
+                this.date = null;
+            }
+
+            this.updateInputs();
+        },
+
+        updateInputs: function(){
+            if (this.date === null) {
+                this.originalInput.val('');
+                this.input.val('');
+                return;
+            }
+
+            this.originalInput.val(this.date.format('YYYY-MM-DD'));
+            this.input.val(this.date.format(this.options.dateFormat));
+        }
+    };
+
+    $.fn.bootstrapDatePicker = function(option) {
+        this.each(function(){
+            var $this   = $(this);
+            var options = typeof(option) === 'object' && option;
+
+            if (!$this.data('datepicker')) {
+                $this.data('datepicker', new DatePicker(this, options));
+            }
+        });
+
+        if (typeof option !== 'string') {
+            return this;
+        }
+
+        if (option === 'getDate') {
+            return this.data('datepicker')['getDate'].call(this.data('datepicker'));
+        }
+
+        if (option === 'setDate') {
+            return this.data('datepicker')['setDate'].call(this.data('datepicker'), arguments[1]);
+        }
+    };
+}(jQuery));
